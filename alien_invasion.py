11c390ac9a925915f8c31278b33b4d9df07e0ea2
Zorina.Kateryna <czorina@gmail.com>
2022-11-17 23:05:42 +0200

    Initial app setup

---
+++ alien_invasion.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+	"""Overall class to manage game assets and behavior."""
+
+	def __init__(self):
+		"""Initialize the game, and create game resources."""
+		pygame.init()
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
+		pygame.display.set_caption("Alien Invasion")
+
+		self.ship = Ship(self)
+
+	def run_game(self):
+		"""Start the main loop for the game."""
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+				self.screen.fill(self.settings.bg_color)
+				self.ship.blitme()
+
+				pygame.display.flip()
+
+
+if __name__ == '__main__':
+	ai = AlienInvasion()
+	ai.run_game()
